c350349073a30276da1b680fda0fcf82fbea653f
Max Lemieux <max.lemieux@gmail.com>
2020-05-11 15:22:32 -0700

    extended Exercise schema to match front end

--- models/Exercise.js
+++ models/Exercise.js
@@ -3,15 +3,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-  author: String,
-  title: String,
+  type: String,
+  name: String,
+  distance: Number,
+  duration: Number,
+  weight: Number,
+  sets: Number,
+  reps: Number,
+  duration: Number,  
   workoutplans: [
     {
       type: Schema.Types.ObjectId,
       ref: "WorkoutPlan"
     }
-  ]
-
+  ],
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
