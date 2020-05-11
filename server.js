0ba625011f3701660cde916a633654ae2ea16f30
Max Lemieux <max.lemieux@gmail.com>
2020-05-10 18:00:01 -0700

    beginnings of exercise routes

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+/*
+db.Library.create({ name: "Campus Library" })
+  .then(dbLibrary => {
+    console.log(dbLibrary);
+  })
+  .catch(({message}) => {
+    console.log(message);
+  });
+
+app.post("/submit", ({body}, res) => {
+  db.Book.create(body)
+    .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(dbLibrary => {
+      res.json(dbLibrary);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+*/
+
+app.get("/exercise", (req, res) => {
+  db.Exercise.findOne({_id: mongojs.ObjectID(req.body._id)}, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(data);
+    }
+  })
+})
+
+
+/*
+app.get("/library", (req, res) => {
+  db.Library.find({})
+    .then(dbLibrary => {
+      res.json(dbLibrary);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/populated", (req, res) => {
+  db.Library.find({})
+    .populate("books")
+    .then(dbLibrary => {
+      res.json(dbLibrary);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+*/
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
