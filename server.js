2aa384c2427337f8660965ec7cbe4175a3a0bcb9
Max Lemieux <max.lemieux@gmail.com>
2020-05-16 21:20:43 -0700

    put the db import back in server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+const db = require('./models');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
