9ad4d3e9e46f70b68208fc382fbd951f5bef2098
Max Lemieux <max.lemieux@gmail.com>
2020-05-17 12:37:48 -0700

    removed .then from workouts/range in favor of c b

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,89 +12,83 @@ module.exports = (app) => {
   });
 
   app.get('/api/workouts/range', (req, res) => {
-    const millisecondsInAWeek = 7 * 24 * 60 * 60 * 1000;
-    console.log(new Date(new Date() - millisecondsInAWeek));
-    // db.Workout.find({ day: { $gte: new Date(new Date() - millisecondsInAWeek) } })
-    db.Workout.find()
-      .then((dbWorkoutRange) => {
-        console.log(dbWorkoutRange);
-        const data = [
-          {
-            dayOfWeek: 'Monday',
-            dayNumber: 1,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-          {
-            dayOfWeek: 'Tuesday',
-            dayNumber: 2,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-          {
-            dayOfWeek: 'Wednesday',
-            dayNumber: 3,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-          {
-            dayOfWeek: 'Thursday',
-            dayNumber: 4,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-          {
-            dayOfWeek: 'Friday',
-            dayNumber: 5,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-          {
-            dayOfWeek: 'Saturday',
-            dayNumber: 6,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-          {
-            dayOfWeek: 'Sunday',
-            dayNumber: 7,
-            totalDuration: 0,
-            totalWeight: 0,
-            exerciseNames: [],
-          },
-        ];
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    // console.log(oneWeekAgo);
+    db.Workout.find({}, (err, docs) => {
+    // db.Workout.find({ day: { $gte: oneWeekAgo } }, (dbWorkoutRange) => {
+      // console.log(docs);
+      const data = [
+        {
+          dayOfWeek: 'Monday',
+          dayNumber: 1,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+        {
+          dayOfWeek: 'Tuesday',
+          dayNumber: 2,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+        {
+          dayOfWeek: 'Wednesday',
+          dayNumber: 3,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+        {
+          dayOfWeek: 'Thursday',
+          dayNumber: 4,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+        {
+          dayOfWeek: 'Friday',
+          dayNumber: 5,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+        {
+          dayOfWeek: 'Saturday',
+          dayNumber: 6,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+        {
+          dayOfWeek: 'Sunday',
+          dayNumber: 7,
+          totalDuration: 0,
+          totalWeight: 0,
+          exerciseNames: [],
+        },
+      ];
 
-        dbWorkoutRange.forEach((workout) => {
-          console.log('workout')
-          console.log(workout)
-          // sunday is day 0, needs to be 7 
-          let dayNumber = workout.day.getDay();
-          if (dayNumber === 0) dayNumber = 7;
-          console.log(`dayOfWeek ${workout.dayOfWeek} and dayNumber ${dayNumber}`);
-          data[dayNumber - 1].totalDuration += workout.totalDuration;
-          data[dayNumber - 1].totalWeight += workout.totalWeight;
-          workout.exercises.forEach((exercise) => {
-            data[dayNumber - 1].exerciseNames.push(exercise.name);
-          });
+      docs.forEach((workout) => {
+        /* Sunday needs to be day 7 */
+        let dayNumber = workout.day.getDay();
+        if (dayNumber === 0) dayNumber = 7;
+        data[dayNumber - 1].totalDuration += workout.totalDuration;
+        data[dayNumber - 1].totalWeight += workout.totalWeight;
+        workout.exercises.forEach((exercise) => {
+          data[dayNumber - 1].exerciseNames.push(exercise.name);
         });
-
-        /* Arrange with today at the end of the array */
-        const toDay = new Date();
-        if (data[6].dayNumber !== toDay.getDay()) {
-          data.unshift(data.pop());
-        }
-
-        res.json(data);
-      })
-      .catch((err) => {
-        res.json(err);
       });
+
+      /* Arrange with today at the end of the array */
+      const toDay = new Date();
+      if (data[6].dayNumber !== toDay.getDay()) {
+        data.unshift(data.pop());
+      }
+
+      return res.json(data);
+    });
   });
 
   app.post('/api/workouts', (req, res) => {
